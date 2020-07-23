2cf639866b45980b6354cff5a00b0971262c2673
umamaheswar reddy <35299489+umamaheswarreddy@users.noreply.github.com>
2020-07-23 12:11:43 +0530

    Zuul-Api-Gateway

---
+++ ZuulApiGateway/src/main/java/com/cts/emp/zuullFilters/ZuulPreLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.cts.emp.zuullFilters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulPreLoggingFilter extends ZuulFilter {
+ 
+	private  Logger logger = LogManager.getLogger(this.getClass());
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request=RequestContext.getCurrentContext().getRequest();
+		logger.info("request -> {} request uri -> {}",request,request.getRequestURI() );
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+	
+
+}
