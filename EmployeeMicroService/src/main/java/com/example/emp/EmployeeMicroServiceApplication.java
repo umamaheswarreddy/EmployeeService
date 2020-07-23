ab7f234ebcc53f8acfd67752cf81991b5f2b19f9
umamaheswar reddy <35299489+umamaheswarreddy@users.noreply.github.com>
2020-07-23 12:06:39 +0530

    Actuator + @EnableErekaClient

--- EmployeeMicroService/src/main/java/com/example/emp/EmployeeMicroServiceApplication.java
+++ EmployeeMicroService/src/main/java/com/example/emp/EmployeeMicroServiceApplication.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.service.Contact;
 
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class EmployeeMicroServiceApplication {
 
 	public static void main(String[] args) {
