8d1f26f4c388bcc9c1965617c5ed7c46c13a5dd7
umamaheswar reddy <35299489+umamaheswarreddy@users.noreply.github.com>
2020-07-30 16:11:04 +0530

    zuul with authentication and autherization

---
+++ ZuulApiGateway/src/main/java/com/cts/emp/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.cts.emp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	Override for Authentication
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+		.withUser("user")
+		.password("user")
+		.roles("USER")
+		.and()
+		.withUser("admin")
+		.password("admin")
+		.roles("ADMIN");
+	
+}
+	
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	
+//	Override for Authorization
+	@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+			.antMatchers("/employee-service/emp/all").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/employee-service/emp/{id}").hasRole("ADMIN")
+			.antMatchers("/").permitAll()
+			.and().formLogin();
+		}
+//	
+	
+	
+	
+	
+}
